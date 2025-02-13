16220445dc6bfe8473e501f340d63c5857dcda34
MiguelGavito <mgavitogzz@gmail.com>
2025-02-13 09:01:21 -0600

    clase de practica de React para probar el uso de componentes

---
+++ src/app/page2/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Rectangle from "../components/Rectangle";
+
+
+export default function Page2() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Rectangle color="green" />
+      <Link href="/" className="text-blue-500 hover:underline">
+        Go to Home
+      </Link>
+    </div>
+  );
+}
