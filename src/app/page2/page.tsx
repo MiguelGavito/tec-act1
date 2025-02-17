38ba2fe44e2c398db0a51e61f8764e26a2b356f8
MiguelGavito <mgavitogzz@gmail.com>
2025-02-16 20:30:56 -0600

    ya cree mi tabla con patron blanco y negro, todavia no se como mover el boton a debajo

--- src/app/page2/page.tsx
+++ src/app/page2/page.tsx
@@ -1,13 +1,39 @@
 import Link from "next/link";
 import Rectangle from "../components/Rectangle";
 
+
+
+
 export default function Page2() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <Rectangle color='green' />
+      <table className="border-collapse">
+        <tbody>
+          <tr>
+            <td className="w-24 h-24 border border-black bg-white text-center"></td>
+            <td className="w-24 h-24 border border-black bg-black text-center"></td>
+            <td className="w-24 h-24 border border-black bg-white text-center"></td>
+          </tr>
+          <tr>
+            <td className="w-24 h-24 border border-black bg-black text-center"></td>
+            <td className="w-24 h-24 border border-black bg-white text-center"></td>
+            <td className="w-24 h-24 border border-black bg-black text-center"></td>
+          </tr>
+          <tr>
+            <td className="w-24 h-24 border border-black bg-white text-center"></td>
+            <td className="w-24 h-24 border border-black bg-black text-center"></td>
+            <td className="w-24 h-24 border border-black bg-white text-center"></td>
+          </tr>
+        </tbody>
+      </table>
+      <a>
+        \n
+      </a>
+      
       <Link href="/" className="text-blue-500 hover:underline">
         Go to Home
       </Link>
+      
     </div>
   );
 }
